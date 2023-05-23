fd60db145f58486124da105f0e176f7527fbcc22
nqd <nqd@users.noreply.github.com>
2023-05-23 21:57:09 +0800

    simple example

--- examples/simple.go
+++ examples/simple.go
@@ -2,21 +2,38 @@
 
 package main
 
-import "github.com/nqd/statum"
+import (
+	"context"
+	"log"
+
+	"github.com/nqd/statum"
+)
 
 func main() {
 	type state string
 	type transaction string
 
 	const (
-		closed state       = "closed"
-		open   state       = "open"
-		open   transaction = "open"
-		close  transaction = "close"
+		sclosed state       = "closed"
+		sopen   state       = "open"
+		topen   transaction = "open"
+		tclose  transaction = "close"
 	)
 
 	config := statum.NewStateMachineConfig[state, transaction]().
-		AddState(open, statum.WithPermit(close, closed)).
-		AddState(closed, statum.WithPermit(open, open))
-	statum.NewFSM[state, transaction](closed, config)
+		AddState(sopen, statum.WithPermit(tclose, sclosed)).
+		AddState(sclosed, statum.WithPermit(topen, sopen))
+	fsm, err := statum.NewFSM[state, transaction](sopen, config)
+	if err != nil {
+		log.Panicln("failed to create new fsm", err)
+	}
+
+	log.Println("Current state:", fsm.Current())
+
+	err = fsm.Fire(context.Background(), tclose)
+	if err != nil {
+		log.Panicln("Failed to create trigger close transaction")
+	}
+
+	log.Println("Current state:", fsm.Current())
 }
